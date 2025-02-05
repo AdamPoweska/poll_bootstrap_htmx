8a8fc9d30e30dfc3451e8f032bbe54920f266454
AdamPoweska <adam.poweska@gmail.com>
2025-02-05 23:13:38 +0100

    probably final commit

--- poll_1/views.py
+++ poll_1/views.py
@@ -1,15 +1,13 @@
 # Create your views here.
 
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import CreateView, DeleteView, TemplateView, ListView, UpdateView
+from django.views.generic import TemplateView, ListView
 from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
 from django.db.models import Count
 from collections import defaultdict
 
 from .forms import *
-from .models import *
+from .models import SurveyOneResponses
 
 
 class MainPageView(TemplateView):
@@ -23,9 +21,10 @@ class QuestionA(FormView):
     form_class = QuizOneQuestionOne
     success_url = reverse_lazy('quiz_1_question_2')
 
+    # zapisanie do modelu i bazy danych, forms nie zapisują do bazy danych - tylko modele
     def form_valid(self, form):
-        answer = form.cleaned_data['question_one_select']
-        SurveyOneResponses.objects.create(question='Question 1', answer=answer) # zapisanie do modelu i bazy danych, forms nie zapisują do bazy danych - tylko modele
+        answer = form.cleaned_data['question_one_select'] # forms "QuizOneQuestionOne" > choicefield "question_one_select" <- przekazanie cleaned data z htmlu do forms
+        SurveyOneResponses.objects.create(question='Question 1', answer=answer) # stworzenie objektu za pomocą modelu SurveyOneResponses
         return super().form_valid(form)
 
 
@@ -50,6 +49,7 @@ class QuestionC(FormView):
         SurveyOneResponses.objects.create(question='Question 3', answer=answer)
         return super().form_valid(form)
 
+
 class SurveyOneResults(ListView):
     model = SurveyOneResponses
     template_name = 'quiz_1_results.html'
@@ -57,28 +57,29 @@ class SurveyOneResults(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        # pobieranie i grupowanie odpowiedzi
+        # Query Set - podejście SQL'a raczej się nie opłaca na tak małą bazę danych
         summary = (
             SurveyOneResponses.objects
-            .values('question', 'answer')
-            .annotate(count=Count('answer'))
+            .values('question', 'answer') # .values pozwala na zwrócenie danych jako słowników a nie jako obiektów modelu
+            .annotate(count=Count('answer')) # .annotate dodaje dodatkowe pola do obiektów pól zwracanych przez zapytania do baz danych (query set) - w tym przypdaku jest to variable count używajacy funckji Count -> która to liczy powiązane obiekty w bazie danych
             .order_by('question')
         )
 
-        # grupowanie odpowiedzi wg. pytań
-        grouped_responses = defaultdict(list)
+        # dict, które przekażemy do html'a
+        grouped_responses = defaultdict(list) # defaultdict(list) - tworzy domyślną wartość jako pustą listę dla nowego klucza -> można dodawać elementy bez sprawdzania czy klucz istnieje
         question_totals = defaultdict(int)
 
-        for response in summary:
-            question = response['question']
-            grouped_responses[question].append(response)
-            question_totals[question] += response['count']
+        for response in summary: # dla każdej odpowiedzi w zbiorze danych z SurveyOneResponses
+            question = response['question'] # bazując na wartości 'question' z każdego 'response'
+            grouped_responses[question].append(response) # dodajemy response do słownika 
+            question_totals[question] += response['count'] # dodajemy count do słownika totals - przekaże liczbę kliknięć w daną odpowiedź
         
         for question, responses in grouped_responses.items():
-            total_responses = question_totals[question]
+            total_responses = question_totals[question] # liczba wszystkich odpowiedzi (.items) dla danego pytania (question) ze słownika question_totals
             for response in responses:
                 response['percentage'] = (response['count'] / total_responses) * 100
         
+        # nadanie ładnych opisów - inaczej będziemy mieć tylko 'Question 1' itd -> z bazy danych
         QUESTION_TITLES = {
             "Question 1": "What do you value most in your work?",
             "Question 2": "What motivates you to work?",
@@ -87,11 +88,11 @@ class SurveyOneResults(ListView):
 
         renamed_grouped_responses = {}
         for question, responses in grouped_responses.items():
-            new_title = QUESTION_TITLES.get(question, question)  # jeśli nic nie będzie to zostanie stara nazwa
-            renamed_grouped_responses[new_title] = responses
+            new_title = QUESTION_TITLES.get(question, question) # .get zwraca wartość dla przekazanego klucza. "value = dictionary.get(key, default_value)" <- dlatego mamy question, question
+            renamed_grouped_responses[new_title] = responses # tworzymy nowy słownik z ładnymi nazwami ale wartościami z poprzedniego słownika
         
         
-        context['grouped_responses'] = dict(renamed_grouped_responses)
+        context['grouped_responses'] = dict(renamed_grouped_responses) # wcześniej używam defaultdict - obiekty tej instancji zachowują się jak słowniki, ale mają domyślną wartość dla nieistniejących kluczy - może to powodować error i dlatego lepiej jeszcze raz zabezpieczyć się "dict()"
         context['question_totals'] = dict(question_totals)
 
         return context
